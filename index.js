492a296c5b7c9c66fdc2dc9cadcd6d5ab3b444c3
Serda Simus <iamserda@gmail.com>
2021-03-29 19:03:16 -0400

    added header

--- index.js
+++ index.js
@@ -29,15 +29,15 @@ async function getAPIs() {
   return data;
 }
 
-function getAPIhtml(myAPI) {
+function getAPIhtml(myAPI, idNum) {
   let { API, Description, Auth, HTTPS, Cors, Link, Category } = myAPI;
   let httpsElem = `<span class="https">${HTTPS}</span>;`;
   let authElem = `<span>${Auth}</span>`;
   let corsElem = `<span class="cors">${Cors}</span>`;
 
   if (!Description) {
-    Description =
-      "No description provided. Click API name for more information.";
+    Description = `No description provided. 
+      Click API name for more information.`;
   }
 
   if (!Auth) {
@@ -63,36 +63,90 @@ function getAPIhtml(myAPI) {
   }
 
   const component = `
-        <div class="api">
-            <h4 class="name"><a class="link" href=${Link} target="_blank">${API}<br><span class="category">${Category}</span></a></h4>
-            <p class="description">Description:<br><span>${Description}</span></p>
-            <p class="auth">Auth: ${authElem}</p>
-            <p class="https">HTTPS: ${httpsElem}</p>
-            <p class="cors">CORS: ${corsElem}</p>
-            <p class="counter"><span>${counter}</span></p>
-        </div>`;
+      <div class="api">
+          <h4 class="name">
+            <a class="link" href=${Link} target="_blank">${API}
+            <br><span class="category">${Category}</span></a></h4>
+
+          <p class="description">Description:
+            <br><span>${Description}</span></p>
+          
+          <p class="auth">Auth: ${authElem}</p>
+          <p class="https">HTTPS: ${httpsElem}</p>
+          <p class="cors">CORS: ${corsElem}</p>
+          <p class="counter"><span>${idNum}</span></p>
+      </div>`;
 
   return component;
 }
+
 function compareNames(a, b) {
   if (a > b) {
     return;
   }
 }
-// displays first 100 apis.
+
+function appHeader() {
+  const header = `
+    <header class="header container">
+      <span class="brand">uiforapi.co</span>
+      <nav class="nav">
+        <ul class="container">
+          <li class="nav-links"><a href="/">home</a></li>
+          <li class="nav-links"><a href="#">p</a></li>
+          <li class="nav-links"><a href="#">home</a></li>
+          <li class="nav-links"><a href="#">home</a></li>
+        </ul>
+      </nav>
+    </header>
+  `;
+
+  return header;
+}
+
+function appFooter() {
+  // using HTML inside of JS is awful. I know.
+  // Creating DOM elements in JS can become unrully, so I do this.
+  const footer = `
+  <footer class="footer">
+      <p>Visit the project's repo 
+          <a href="https://github.com/davemachado/public-api">here</a></p>
+      <p>Many thanks to <a href="https://github.com/davemachado" 
+          target="_blank">Dave Machado</a> and the contributers at: 
+          <a href="https://api.publicapis.org/" 
+          target="_blank>publicapis.org</a></p>
+      <p>🇭🇹 Made with ❤️ in Inwood, NYC. 🇭🇹</p>
+  </footer>
+  `;
+
+  return footer;
+}
+
+// displays all object within the myAPIs array.
 function displayAPIs(myAPIs) {
+  let counter = 0;
+  const { entries } = myAPIs;
   const app = document.createElement("div");
+  let components = "";
+
   app.id = "app";
   app.classList.add("app-grid");
-  document.body.prepend(app);
 
-  const { entries } = myAPIs;
-  console.log(entries);
+  //adding a header to app
+  document.body.innerHTML += appHeader();
+  document
+    .getElementsByTagName("header")
+    .item(0)
+    .insertAdjacentElement("afterend", app);
+
   entries.forEach((item) => {
-    let counter = 0;
-    console.log(++counter, item);
-    // app.innerHTML += getAPIhtml(item, ++counter);
+    // to limit strain on the browser and DOM rendering
+    // I have accumlated the components here.
+    // Below, I will make a single DOM insertion.
+    components += getAPIhtml(item, ++counter);
   });
+
+  app.innerHTML += components;
 }
 
 getAPIs()
