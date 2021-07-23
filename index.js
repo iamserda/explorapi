8eafaceb51c661ec3e1ef5603a1112845f7a6762
Serda Simus <iamserda@gmail.com>
2021-07-23 15:36:11 -0400

    updates to index.js

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
-/* 
-    Bonus Challenge 
-
-    Fetch the list of 642 open APIs from
-        https://api.publicapis.org/entries
-        
-    Create a my-api component
-        display the name and category of the API,
-        the description, and also display the type 
-        of Auth (if any) and whether or not the API 
-        supports HTTPS
-    
-    Use CSS Grid to style my-api
-        The title and category should be 
-        listed as Title (Category) 
-        and should link to the API docs
-        
-    The grid should have 4 rows
-        3rem, 1rem, 4rem, 3rem respectively
-        and 3 columns each 1/3rd of available width
-        
-    Finally, display all of the APIs
-*/
-
 async function getAPIs() {
   const url = "https://api.publicapis.org/entries";
   const apiRequest = await fetch(url);
   const data = await apiRequest.json();
-  const { entries } = data;
+  const {
+    entries
+  } = data;
 
   entries.sort((api1, api2) => {
     var apiName1 = api1.API.toUpperCase();
@@ -50,7 +28,15 @@ async function getAPIs() {
 }
 
 function getAPIhtml(myAPI, idNum) {
-  let { API, Description, Auth, HTTPS, Cors, Link, Category } = myAPI;
+  let {
+    API,
+    Description,
+    Auth,
+    HTTPS,
+    Cors,
+    Link,
+    Category
+  } = myAPI;
   let httpsElem = `<span class="https">${HTTPS}</span>;`;
   let authElem = `<span>${Auth}</span>`;
   let corsElem = `<span class="cors">${Cors}</span>`;
